b07eab0671d0221b6e2ea5e2bfd58b3d7cc74b7b
Tahsin Sumiya <Tahsinadity187@gmail.com>
2023-10-31 01:04:58 +0600

    card modified

--- frontend/src/component/CategoryItem.jsx
+++ frontend/src/component/CategoryItem.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {BsFillPlayFill} from "react-icons/bs";
+
 export default function CategoryItem({ item }) {
   return (
-    <div className="card" >
-      <img className="card-img-top" src={item.img} style={{"height":"50vh"}} alt="Card image cap h-75"/>
-      <div className="card-body  ">
-        <div className='d-flex '>
-             <p className="card-text text-sm text-secondary">{item.title}</p>
-        <button className='btn  btn-sm h-25  btn-outline-secondary
-         text-black border-white 
-          mx-5'>Shop now <BsFillPlayFill/></button></div>
-       
-      </div>
+    <div className="card">
+    <img className="card-img-top" src={item.img} style={{"height":"50vh"}} alt="Card image cap h-75" />
+    <p className="card-text text-sm text-secondary text-center">{item.title}</p>
+    <div className='d-flex justify-content-center align-items-center'>
+      <button className='btn btn-sm h-25 btn-outline-secondary
+       text-black border-white'>
+        Shop now <BsFillPlayFill />
+      </button>
     </div>
+  </div>
+  
     
   );
 }
